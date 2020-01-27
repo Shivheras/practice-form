0da473173e3a40cfe1b6eebb49f7686cf9700206
Shivheras <shivheras@gmail.com>
2020-01-27 15:17:13 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,14 @@
-import { Component ,OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component , } from '@angular/core';
+import  { user } from './user';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  implements OnInit {
-  registerForm: FormGroup;
-submitted = false;
-
-constructor(private formBuilder: FormBuilder) { }
- 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            fullName:[''],
-             email:[''],
-             password:[''],
-            rePassword:[''],
-              altEmail:[''],
-             mobileNo:[''],
-             day:[''],
-              month:[''],
-               year:[''],
-                gender:[''],
-               country:[''],
-                  city:['']
-  
-
-        });
-    }
- 
+export class AppComponent  implements  {
+ user:User = new User();
     submit()
-  {
-    console.log(this.userFormGroup.value);
+     {
+    console.log(this.user);
+      }
   }
-}
